ea8b8d03e10fdf383843970488bb6dde186e1cab
KronoMuldor <kmd@deb9vm64b>
2017-03-24 23:20:00 +0100

    Correction des chemins relatifs

--- test/test.py
+++ test/test.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
-# commentaire
-# obtenir une représentation concise des utilisateurs qui ont aimés le plus un film donné
-def UserProfileFilmID(data, FilmID):
-    filmdb = data.groupby("movie_id")
-    
-    #meaneval = data.
-    return;
 
-codedir = 'sample_code/'                        # Change this to the directory where you put the code
+codedir = '../sample_code/'                        # Change this to the directory where you put the code
 from sys import path; path.append(codedir)
 #%matplotlib inline
 import seaborn as sns; sns.set()
-datadir = 'public_data/'                        # Change this to the directory where you put the input data
+datadir = '../public_data/'                        # Change this to the directory where you put the input data
 dataname = 'movierec'
 basename = datadir  + dataname
 import data_io
