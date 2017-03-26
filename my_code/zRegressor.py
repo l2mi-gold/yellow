2f4db113422262ce0aa59fc2e2243cb209cfca73
l2mi-gold <gold@chalearn.org>
2017-03-26 16:06:14 +0200

    Update zRegressor.py

--- my_code/zRegressor.py
+++ my_code/zRegressor.py
@@ -1,14 +1,21 @@
 from sklearn.base import BaseEstimator
+from zPreprocessor import Preprocessor
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import BaggingClassifier, VotingClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeRegressor
 import pickle
 
 
 class Regressor(BaseEstimator):
 	def __init__(self):
-		pass
+		self.clf = VotingClassifier(estimators=[
+                ('basic1', DecisionTreeRegressor()), 
+                ('basic2', LinearRegression())], 
+                voting='soft')  
 
 	def fit(self, X, y):
-		self.clf = DecisionTreeRegressor()
 		self.clf.fit(X, y)
 
 	def predict(self, X):
