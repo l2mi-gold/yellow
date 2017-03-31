c1b55ae757bc612fbcd77c534b2c15fc3fdd5d8a
l2mi-gold <gold@chalearn.org>
2017-03-31 16:42:38 +0200

    ajout exemples d'utilisateurs

--- my_code/zDataManagerTest.py
+++ my_code/zDataManagerTest.py
@@ -15,8 +15,9 @@ print D
 D.DataStats('train')
 #D.ShowScatter(1, 2, 'train')
 D.ShowUserProfile(5103)
+D.ShowUserProfile(5113)
+D.ShowUserProfile(4104)
 D.ShowCorrelationMatrix()
-
 
 assert D.UserRatingMovieID(100000, 100000) == 0
 
@@ -35,7 +36,6 @@ assert D.CategoryAverageRatingMovieID('job_other', 100000) == 0
 assert D.CategoryAverageRatingMovieID('job_other', 3174) <= 5
 # Moyenne supérieur à 0
 assert D.CategoryAverageRatingMovieID('job_other', 3174) >= 0
-
 
 # CategoryAverageRatingMovieGenre :
 
